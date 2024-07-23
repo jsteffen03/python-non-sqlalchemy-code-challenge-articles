16513055eaa6c370323916cad80d679c5f6d3c81
Justin Steffen <jcsteffen03@gmail.com>
2024-07-22 19:06:27 -0600

    first test passed

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,10 +3,32 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
+
+    def get_title(self):
+        return self._title
+    
+    def set_title(self, value):
+        if type(value) is str and 5 <= len(value) <= 50 and not hasattr(self, "title"):
+            self._title = value 
+        else: 
+            print("No!")
+        
+    title = property(get_title, set_title)
         
 class Author:
     def __init__(self, name):
         self.name = name
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, value):
+        if type(value) is str and 0 < len(value) and not hasattr(self, "name"):
+            self._name = value 
+        else: 
+            print("No!")
+        
+    name = property(get_name, set_name)
 
     def articles(self):
         pass
@@ -24,6 +46,28 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, value):
+        if type(value) is str and 2 <= len(value) <= 16:
+            self._name = value 
+        else: 
+            print("No!")
+        
+    name = property(get_name, set_name)
+
+    def get_category(self):
+        return self._category
+    
+    def set_category(self, value):
+        if type(value) is str and 0 < len(value):
+            self._category = value 
+        else: 
+            print("No!")
+        
+    category = property(get_category, set_category)
 
     def articles(self):
         pass
